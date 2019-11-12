d661ce1ba840596a2f95860e65fb49d3e5a2760a
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-11-12 15:46:23 +0100

    task manager ugly but completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # create
+  post 'tasks', to: 'tasks#create'
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id/', to: 'tasks#destroy', as: 'delete'
+  # delete
 end
